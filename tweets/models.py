4c9595515cca9612f78eae7d3a1099c88efa2dbf
Hasan Zonouzi <hasanzon@yahoo.com>
2023-10-24 13:05:25 +0330

    feat: complete tweet model and migration

--- tweets/models.py
+++ tweets/models.py
@@ -10,8 +10,15 @@ class Tweet(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     likes = models.ManyToManyField("users.User", related_name="liked")
     bookmark = models.ManyToManyField("users.User", related_name="bookmarked")
-    # retweet = models.ManyToManyField("users.User", related_name="retweeted")
-    # quoted_tweet
+    retweet = models.ManyToManyField("users.User", through="tweets.RetweetUser", related_name="retweeted")
+    quoted_tweet = models.URLField()
     parent = models.ForeignKey("tweets.Tweet", related_name="comments",
                                on_delete=models.SET_NULL, null=True, blank=True)
-    # mentioned_users = models.ManyToManyField("users.User")
+    mentioned_users = models.ManyToManyField("users.User", related_name="mentioned_tweets")
+
+
+class RetweetUser(models.Model):
+    tweet = models.ForeignKey("tweets.Tweet", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    retweet_date = models.DateTimeField(auto_now_add=True)
+
