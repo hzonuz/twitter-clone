3c540a547cdb110d8227dfd48ec6c5a183325e96
Hasan Zonouzi <hasanzon@yahoo.com>
2023-10-24 13:05:36 +0330

    feat: drf added to project

--- server/settings.py
+++ server/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
     "users",
     "tweets",
 ]
@@ -110,6 +112,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 AUTH_USER_MODEL = "users.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication"
+    ],
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",)
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
